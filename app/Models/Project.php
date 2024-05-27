596012a0c489686a0439abc93f22913554fb5dfa
Daniele <danielecolaci@outlook.com>
2024-05-27 16:48:15 +0200

    Add Migration for pivot table project technology. Add many to many relationship between projects and technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Type;
+use App\Models\Technology;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -29,5 +32,15 @@ class Project extends Model
     public function type(): BelongsTo
     {
         return $this->belongsTo(Type::class);
+    }
+
+    /**
+     * The roles that belong to the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
